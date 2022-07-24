15429edf06e0b16838cfe63ba6e235f196b8f773
Antis <Antis28@mail.ru>
2022-07-24 22:52:00 +0300

    Добавил сервер

---
+++ AspCoreServer/Controllers/MessagerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagerController : ControllerBase
+    {
+
+        // GET api/<MessagerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value {id.ToString()}";
+        }
+
+        // POST api/<MessagerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
