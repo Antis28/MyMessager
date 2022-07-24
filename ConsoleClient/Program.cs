f88d973bcc0fcf33dcb4639964f9664903e5e8c1
Antis <Antis28@mail.ru>
2022-07-24 22:11:58 +0300

    Добавил сериализацию в Json

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ConsoleClient;
+using Newtonsoft.Json;
 
 namespace Messenger
 {
@@ -7,8 +8,13 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message(userName: "Antis", messageText: "Hi", timeStamp: DateTime.Now);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message desOutput = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(desOutput);
+            // { "UserName":"Antis","MessageText":"Hi","TimeStamp":"2022-07-24T22:11:11.6134272+03:00"}
+            // Antis < 24.07.2022 22:11:11 >: Hi
         }
     }
 }
