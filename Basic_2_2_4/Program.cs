46e2fb213e15b73db001c5f53d0de7cdb114b22d
Antis <Antis@HOME-PC>
2022-07-24 19:28:39 +0300

    Добавьте файлы проекта.

---
+++ Basic_2_2_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sInput = "Мне нравится этот курс! \":)\"";     //Console.ReadLine();
+            var s = sInput.Split(' ');
+            var result = new StringBuilder();
+            foreach (var item in s)
+            {
+                result.AppendLine(item + " ");
+            }
+            Console.WriteLine(result.ToString().Trim());
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = {1, 8, 10, 0, -4, 5};
+            SortArray(numbers);
+        }
+
+        private static void SortArray(int [] nums)
+        {
+            var l = nums.ToList();
+            l.Sort();
+            foreach (var item in l)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
