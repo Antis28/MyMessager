947399c119f174f2e2a897997147814e03ba2a37
Antis <Antis28@mail.ru>
2022-07-24 23:41:02 +0300

    добавил обработку GET и POST запросов

--- AspCoreServer/Controllers/MessagerController.cs
+++ AspCoreServer/Controllers/MessagerController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConsoleClient;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,18 +12,31 @@ namespace AspCoreServer.Controllers
     [ApiController]
     public class MessagerController : ControllerBase
     {
-
+        private static List<Message> messages = new List<Message>();
         // GET api/<MessagerController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return $"value {id.ToString()}";
+            string outputString = "Not found";
+            if (id < messages.Count && id >= 0)
+            {
+                outputString = JsonConvert.SerializeObject(messages[id]);
+            }
+            Console.WriteLine($"Запрошено сообщение #{id}: {outputString}");
+            return outputString;
         }
 
         // POST api/<MessagerController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if(msg == null)
+            {
+                return BadRequest();
+            }
+            messages.Add(msg);
+            Console.WriteLine($"Всего сообщений: {messages.Count}, посланное сообщение: {msg}");
+            return new OkResult(); 
         }
     }
 }
